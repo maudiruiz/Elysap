6a5aea6d708b0c432de2e9fc1a7d2b20c3ac93a2
root <root@Elysa.(none)>
2014-01-05 06:24:58 +0000

    Solo cambie la ruta de home y agregue display controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Myapp::Application.routes.draw do
+  get "display/index"
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root to: 'home#index'
+root to: 'display#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
