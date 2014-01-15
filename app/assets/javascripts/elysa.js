c7f9d284fdf096b516caa36835ca30bfbabf98ec
Mauricio Ruiz <maudiruiz@hotmail.com>
2014-01-15 01:30:32 -0600

    Modifique todo el layout del home

---
+++ app/assets/javascripts/elysa.js
@@ -0,0 +1,8 @@
+$( "#formregistro" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+function showregistro(){
+  console.log("OKA");
+  $("#formregistro").toggleClass( "show" );
+}
