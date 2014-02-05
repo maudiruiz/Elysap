cb7eee538790aa263c0e060ac097c1b025cd9237
Mauricio Ruiz <maudiruiz@hotmail.com>
2014-02-05 04:35:36 -0600

    git ignore salvanos

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module Myapp
   class Application < Rails::Application
-    config.i18n.enforce_available_locales = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
