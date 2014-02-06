1fe58ffd5af69adf14b989d8c64b7af71d13a664
Mauricio Ruiz <maudiruiz@hotmail.com>
2014-02-05 18:46:05 -0600

    Cambie inspect

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 	  user = User.where(:provider => auth.provider, :uid => auth.uid).first
 	  unless user
 	  	# logger.info auth
-	    user = User.create(name:auth.extra.raw_info.name.to_text.inspect,
+	    user = User.create(name:auth.extra.raw_info.name.inspect,
 	                         provider:auth.provider,
 	                         uid:auth.uid,
 	                         gender:auth.extra.raw_info.gender,
